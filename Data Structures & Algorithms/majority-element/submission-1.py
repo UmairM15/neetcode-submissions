434085b6193f42cd5dbbe041077b507c341f2df3
Umair Juned Mulla <64334746+UmairM15@users.noreply.github.com>
2026-05-19 19:49:42 -0400

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        seen = defaultdict(int)
+        majorityCutoff = len(nums) // 2
+
+        for n in nums:
+            seen[n] += 1
+
+            if seen[n] > majorityCutoff:
+                return n
+            
