8bee072ca722f88951cc64888fe6afe3c681f2d5
Umair Juned Mulla <64334746+UmairM15@users.noreply.github.com>
2026-06-05 22:58:19 -0400

    Add: boats-to-save-people - submission-4

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        boatCount = 0
+        people.sort()
+        i, j = 0, len(people) - 1
+        
+        while i < j:
+            totalWeight = people[j] + people[i]
+
+            if people[j] >= limit or totalWeight > limit:
+                boatCount += 1
+                j -= 1
+            else:
+                boatCount += 1
+                i += 1
+                j -= 1
+
+            if i == j:
+                boatCount += 1
+        
+        return boatCount
