c545f4a60cc9f3358ffd0b65314d6b3bb6ba2bc3
Umair Juned Mulla <64334746+UmairM15@users.noreply.github.com>
2026-06-09 16:46:34 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tCount = defaultdict(int)
+        for c in t:
+            tCount[c] += 1
+        need = len(tCount)
+
+        minLen = float("inf")
+        minL, minR = 0, len(s)
+        sCount = defaultdict(int)
+        l = cur = 0
+        for r in range(len(s)):
+            rightChar = s[r]
+            sCount[rightChar] += 1
+            if sCount[rightChar] == tCount[rightChar]:
+                cur += 1
+            while cur == need:
+                leftChar = s[l]
+                if minLen > r - l + 1:
+                    minLen = r - l + 1
+                    minL = l
+                    minR = r
+                if sCount[leftChar] == tCount[leftChar] and tCount[leftChar] != 0:
+                    cur -= 1
+                sCount[leftChar] -= 1
+                l += 1
+
+        return "" if minLen == float("inf") else s[minL:minR + 1]
+
+        
