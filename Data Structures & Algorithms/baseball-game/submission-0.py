cb5a21921e32305a45ff7db356956738d4b3e25f
Umair Juned Mulla <64334746+UmairM15@users.noreply.github.com>
2026-06-09 22:34:29 -0400

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        rec = []
+        j = curSum = 0
+        for i in range(len(operations)):
+            curOp = operations[i]
+            if curOp == "+":
+                newScore = rec[j - 1] + rec[j - 2]
+                curSum += newScore
+                rec.append(newScore)
+                j += 1
+            elif curOp == "D":
+                rec.append(rec[j - 1] * 2)
+                curSum += rec[j]
+                j += 1
+            elif curOp == "C":
+                curSum -= rec[j - 1]
+                rec.pop()
+                j -= 1
+            else:
+                rec.append(int(curOp))
+                curSum += int(curOp)
+                j += 1
+        return curSum
