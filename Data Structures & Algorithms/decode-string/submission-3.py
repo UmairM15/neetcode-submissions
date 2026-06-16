b32b9a587ac5ea92b0aaf8d8770589204a316669
Umair Juned Mulla <64334746+UmairM15@users.noreply.github.com>
2026-06-16 11:02:12 -0400

    Add: decode-string - submission-3

---
+++ Data Structures & Algorithms/decode-string/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c == "]":
+                substring = ""
+                multiplier = 0
+                decimalPlace = 1
+                while stack[-1] != "[":
+                    start = stack.pop()
+                    substring = start + substring
+                stack.pop() # remove "["
+                while stack and stack[-1].isnumeric():
+                    multiplier += + int(stack.pop()) * decimalPlace
+                    decimalPlace *= 10
+                stack.append(multiplier * substring)
+            else:
+                stack.append(c)
+        return "".join(stack)
