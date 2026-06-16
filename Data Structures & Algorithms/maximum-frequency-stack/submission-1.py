528acf0105eedee72eb2eeead73002c92e350720
Umair Juned Mulla <64334746+UmairM15@users.noreply.github.com>
2026-06-16 12:57:11 -0400

    Add: maximum-frequency-stack - submission-1

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-1.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class FreqStack:
+
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.group = defaultdict(list)
+        self.maxFreq = 0
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        f = self.freq[val]
+
+        self.maxFreq = max(self.maxFreq, f)
+        self.group[f].append(val)
+
+    def pop(self) -> int:
+        val = self.group[self.maxFreq].pop()
+        self.freq[val] -= 1
+
+        if not self.group[self.maxFreq]:
+            self.maxFreq -= 1
+
+        return val
