845710fdabe256a932a2a60214aee6f5442e3ed6
Umair Juned Mulla <64334746+UmairM15@users.noreply.github.com>
2026-06-18 22:08:44 -0400

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+        while l <= r:
+            m = l + (r - l) // 2
+            sqrVal = m * m
+            print(l)
+            print(r)
+            print(m, "\n")
+            if sqrVal <= x and (m + 1) * (m + 1) > x:
+                return m
+            elif sqrVal > x:
+                r = m - 1
+            else:
+                l = m + 1
+        return l
